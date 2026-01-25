b79952abd74f1b2c080ed5880f31b429bd42b1ed
ajlongcoy21 <ajlongcoy21@gmail.com>
2026-01-25 15:01:51 -0500

    running code on clanker

--- src/main/java/frc/robot/Actors/Subsystems/Vision.java
+++ src/main/java/frc/robot/Actors/Subsystems/Vision.java
@@ -20,37 +20,37 @@ public class Vision extends SubsystemBase {
 
     public void getLimelightFrontPose() {
         // "limelight-front", "limelight-back", "limelight-left", "limelight-right"
-        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiRed("limelight-front");
+        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-front");
         System.out.println("Front-pose: " + mt1.pose);
         System.out.println("Front-timestamp(S): " + mt1.timestampSeconds);
     }
 
     public void getLimelightBackPose() {
         // "limelight-front", "limelight-back", "limelight-left", "limelight-right"
-        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiRed("limelight-back");
+        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-back");
         System.out.println("Back-pose: " + mt1.pose);
         System.out.println("Back-timestamp(S): " + mt1.timestampSeconds);
     }
 
     public void getLimelightLeftPose() {
         // "limelight-front", "limelight-back", "limelight-left", "limelight-right"
-        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiRed("limelight-left");
+        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-left");
         System.out.println("Left-pose: " + mt1.pose);
         System.out.println("Left-timestamp(S): " + mt1.timestampSeconds);
     }
 
     public void getLimelightRightPose() {
         // "limelight-front", "limelight-back", "limelight-left", "limelight-right"
-        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiRed("limelight-right");
+        LimelightHelpers.PoseEstimate mt1 = LimelightHelpers.getBotPoseEstimate_wpiBlue("limelight-right");
         System.out.println("Right-pose: " + mt1.pose);
         System.out.println("Right-timestamp(S): " + mt1.timestampSeconds);
     }
 
     @Override
     public void periodic() {
-        this.getLimelightBackPose();
-        this.getLimelightFrontPose();  
-        this.getLimelightRightPose();
-        this.getLimelightLeftPose();  
+        // this.getLimelightBackPose();
+        // this.getLimelightFrontPose();  
+        // this.getLimelightRightPose();
+        // this.getLimelightLeftPose();  
     }
 }
