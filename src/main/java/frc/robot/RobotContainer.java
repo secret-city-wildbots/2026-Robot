2cbd6c313c4067707b89b07d267b5c3e73505b5c
Jasper Springer <jasperwspringer@gmail.com>
2026-02-14 15:11:05 -0500

    fixed error

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -33,12 +33,16 @@ import frc.robot.Commands.Turret.TrackHubCommand;
 
 public class RobotContainer {
     // TODO: Set max speed back to normal
-    // private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond); // kSpeedAt12Volts desired top speed
+    // private double MaxSpeed = TunerConstants.kSpeedAt12Volts.in(MetersPerSecond);
+    // // kSpeedAt12Volts desired top speed
     private double MaxSpeed = 1.0; // kSpeedAt12Volts desired top speed
 
     // TODO: Set max rotation back to normal
-    // private double MaxAngularRate = RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
-    private double MaxAngularRate = RotationsPerSecond.of(0.5).in(RadiansPerSecond); // 3/4 of a rotation per second max angular velocity
+    // private double MaxAngularRate =
+    // RotationsPerSecond.of(0.75).in(RadiansPerSecond); // 3/4 of a rotation per
+    // second max angular velocity
+    private double MaxAngularRate = RotationsPerSecond.of(0.5).in(RadiansPerSecond); // 3/4 of a rotation per second max
+                                                                                     // angular velocity
     /* Setting up bindings for necessary control of the swerve drive platform */
     private final SwerveRequest.FieldCentric drive = new SwerveRequest.FieldCentric()
             .withDeadband(MaxSpeed * 0.1).withRotationalDeadband(MaxAngularRate * 0.1) // Add a 10% deadband
@@ -54,21 +58,20 @@ public class RobotContainer {
 
     private final Turret flturret = new Turret(44, 0);
 
-      /* Path follower */
+    /* Path follower */
     private final SendableChooser<Command> autoChooser;
-    
+
+    public final Shooter shooter = new Shooter();
+
+    public final Indexer indexer = new Indexer();
+
     public RobotContainer() {
-        public final Vision vision = new Vision();
 
-        public final Shooter shooter = new Shooter();
-
-        public final Indexer indexer = new Indexer();
-      
         // TODO: Set default auto
         autoChooser = AutoBuilder.buildAutoChooser("Reverse 9");
         SmartDashboard.putData("Auto Mode", autoChooser);
 
-        drivetrain.resetPose(new Pose2d( new Translation2d(2,2), new Rotation2d()));
+        drivetrain.resetPose(new Pose2d(new Translation2d(2, 2), new Rotation2d()));
         configureBindings();
 
         // Warmup PathPlanner to avoid Java pauses
@@ -79,13 +82,14 @@ public class RobotContainer {
         // Note that X is defined as forward according to WPILib convention,
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
-            // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
-        );
+                // Drivetrain will execute this command periodically
+                drivetrain.applyRequest(() -> drive.withVelocityX(-joystick.getLeftY() * MaxSpeed) // Drive forward with
+                                                                                                   // negative Y
+                                                                                                   // (forward)
+                        .withVelocityY(-joystick.getLeftX() * MaxSpeed) // Drive left with negative X (left)
+                        .withRotationalRate(-joystick.getRightX() * MaxAngularRate) // Drive counterclockwise with
+                                                                                    // negative X (left)
+                ));
 
         flturret.setDefaultCommand(new TrackHubCommand(flturret, drivetrain::getPose));
 
@@ -93,13 +97,11 @@ public class RobotContainer {
         // neutral mode is applied to the drive motors while disabled.
         final var idle = new SwerveRequest.Idle();
         RobotModeTriggers.disabled().whileTrue(
-            drivetrain.applyRequest(() -> idle).ignoringDisable(true)
-        );
+                drivetrain.applyRequest(() -> idle).ignoringDisable(true));
 
         joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
-            point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        joystick.b().whileTrue(drivetrain.applyRequest(
+                () -> point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))));
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
@@ -119,6 +121,6 @@ public class RobotContainer {
         /* Run the path selected from the auto chooser */
         return autoChooser.getSelected();
 
-        //return Commands.print("No autonomous command configured");
+        // return Commands.print("No autonomous command configured");
     }
 }
