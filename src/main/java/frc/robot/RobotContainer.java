f4e06cd899e9a3f90df598a7144410b51658d625
WildRod <wildrodcoding@gmail.com>
2026-03-17 14:19:36 -0400

    added new paths; shot smoothing; lobbing; flexible PID tuning

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,13 +17,18 @@ import com.pathplanner.lib.events.EventTrigger;
 
 // Import WPILib Librarires
 import static edu.wpi.first.units.Units.*;
+
+import java.util.function.Supplier;
+
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Direction;
@@ -69,6 +74,10 @@ public class RobotContainer {
         .withDriveRequestType(DriveRequestType.OpenLoopVoltage); // Use open-loop control for drive motors
     private final SwerveRequest.SwerveDriveBrake brake = new SwerveRequest.SwerveDriveBrake();
     private final SwerveRequest.PointWheelsAt point = new SwerveRequest.PointWheelsAt();
+
+    private double xVelAvg = 0.0;
+    private double yVelAvg = 0.0;
+    private double hVelAvg = 0.0;
 
     private final Telemetry logger = new Telemetry(MaxSpeed);
 
@@ -129,12 +138,32 @@ public class RobotContainer {
         // and Y is defined as to the left according to WPILib convention.
         drivetrain.setDefaultCommand(
             // Drivetrain will execute this command periodically
-            drivetrain.applyRequest(() ->
-                drive.withVelocityX(-JoystickScaler.scaleStrafe(joystick.getLeftY()) * MaxSpeed) // Drive forward with negative Y (forward)
-                    .withVelocityY(-JoystickScaler.scaleStrafe(joystick.getLeftX()) * MaxSpeed) // Drive left with negative X (left)
-                    .withRotationalRate(-JoystickScaler.scaleRotate(joystick.getRightX()) * MaxAngularRate) // Drive counterclockwise with negative X (left)
-            )
+            drivetrain.applyRequest(new Supplier<SwerveRequest>() {
+                public SwerveRequest get() {
+                    double inputX = joystick.getLeftY();
+                    double inputY = joystick.getLeftX();
+                    double inputH = joystick.getRightX();
+                    xVelAvg = xVelAvg+(inputX*0.1)/1.1;
+                    yVelAvg = yVelAvg+(inputY*0.1)/1.1;
+                    hVelAvg = hVelAvg+(inputH*0.1)/1.1;
+                    if (drivetrain.getPose().getX() < 4.25 && joystick.getRightTriggerAxis() > 0.4) {
+                        inputX = xVelAvg;
+                        inputY = yVelAvg;
+                        inputH = hVelAvg;
+                    }
+                    return drive.withVelocityX(-JoystickScaler.scaleStrafe(inputX) * MaxSpeed) // Drive forward with negative Y (forward)
+                    .withVelocityY(-JoystickScaler.scaleStrafe(inputY) * MaxSpeed) // Drive left with negative X (left)
+                    .withRotationalRate(-JoystickScaler.scaleRotate(inputH) * MaxAngularRate); // Drive counterclockwise with negative X (left)
+
+                }
+            })
         );
+
+        joystick.rightTrigger(0.4).onTrue(new FunctionalCommand(null, () -> {
+            xVelAvg = joystick.getLeftY();
+            yVelAvg = joystick.getLeftX();
+            hVelAvg = joystick.getRightX();
+        }, null, null, new Subsystem[0]));
 
         shooter.setDefaultCommand(new AimAtHubCommand(shooter, turret, drivetrain::getPose, () -> {
             var state = drivetrain.getState();
@@ -152,12 +181,12 @@ public class RobotContainer {
         );
 
         // Break when pressing A
-        joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
+        //joystick.a().whileTrue(drivetrain.applyRequest(() -> brake));
 
         // Orientate wheels when pressing B and and moving left and right joysticks
-        joystick.b().whileTrue(drivetrain.applyRequest(() ->
+        /*joystick.b().whileTrue(drivetrain.applyRequest(() ->
             point.withModuleDirection(new Rotation2d(-joystick.getLeftY(), -joystick.getLeftX()))
-        ));
+        ));*/
 
         // Run SysId routines when holding back/start and X/Y.
         // Note that each routine should be run exactly once in a single log.
